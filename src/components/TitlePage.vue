c26d884dfea2c3b00019f222d5704c87cf30e4fc
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-06-07 16:59:09 +0200

    finalizzazione esercizio

---
+++ src/components/TitlePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h2>La mia prima app con Vue CLI</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitlePage',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h2 {
+  margin: 40px 0 0;
+}
+</style>
